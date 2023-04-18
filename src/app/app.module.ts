e7b68add3936a5411f6a6d58f46aa92f14bcedf4
Genady kogan <rgkogan@gmail.com>
2023-04-18 21:34:18 +0300

    routing start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EducationComponent } from './education/education.component';
+import { HomePageComponent } from './home-page/home.page.component';
+import { PersonalDetailsComponent } from './personal-details/personal.details.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes: Routes =[
+  {path: '', component: HomePageComponent},
+  {path: 'education', component: EducationComponent},
+  {path: 'personal-details', component: PersonalDetailsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PersonalDetailsComponent,
+    EducationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
