0ce46df326ccf97e25d2306197cea90db8886ba7
GenadyKogan <rgkogan@gmail.com>
2023-04-19 13:12:08 +0300

    button in home page

--- src/app/home-page/home.page.component.ts
+++ src/app/home-page/home.page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-home-page',
@@ -7,5 +8,9 @@ import { Component } from "@angular/core";
 
 })
 export class HomePageComponent{
+    constructor(private router: Router){}
+    onLoadServers(){
+        this.router.navigate(['/personal-details'])
+    }
 
 }
