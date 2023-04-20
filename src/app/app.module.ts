5c0e6d4ae9e32f4c23700410a1bcb1ba6e815916
Genady kogan <rgkogan@gmail.com>
2023-04-20 23:33:36 +0300

    useHash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ const appRoutes: Routes =[
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    [RouterModule.forRoot(appRoutes, { useHash: true })]
     
   ],
   exports: [RouterModule],
