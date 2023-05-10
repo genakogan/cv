15a98db9483d18809ef78d69e20ac448c6c78bae
GenadyKogan <rgkogan@gmail.com>
2023-05-10 14:16:12 +0300

    Sticky navigation started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css', './app.queries.css']
 })
 export class AppComponent {
+  constructor(private elRef: ElementRef) {}
+
+  // Sticky navigation
+  ngAfterViewInit() {
+    const sectionMainHeroEl = this.elRef.nativeElement.querySelector(".main-hero")
+    const obs = new IntersectionObserver(function(){},{});
+    obs.observe(sectionMainHeroEl);
+    
+    
+    
+    
+    const myName = 'Gena';
+    const h1 = this.elRef.nativeElement.querySelector('.heading-primary');
+   
+    h1.style.color = '#8695b7';
+    h1.style.fontSize = '60px';
+    h1.addEventListener('click', this.handleClick);
+  }
+  handleClick(event: Event) {
+    console.log('h1 clicked!');
+  }
 }
--- src/app/home-page/home.page.component.ts
+++ src/app/home-page/home.page.component.ts
@@ -1,13 +1,23 @@
-import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-home-page',
-    templateUrl: './home.page.component.html',
-    styleUrls: ['./home.page.component.css', './home.page.queries.css']
-
+  selector: 'app-home-page',
+  templateUrl: './home.page.component.html',
+  styleUrls: ['./home.page.component.css', './home.page.queries.css'],
 })
-export class HomePageComponent{
-   
+export class HomePageComponent {
+  constructor(private elRef: ElementRef) {}
 
+  ngAfterViewInit() {
+    const myName = 'Gena';
+    const h1 = this.elRef.nativeElement.querySelector('.heading-primary');
+   
+    h1.style.color = '#8695b7';
+    h1.style.fontSize = '60px';
+    h1.addEventListener('click', this.handleClick);
+  }
+  handleClick(event: Event) {
+    console.log('h1 clicked!');
+  }
 }
