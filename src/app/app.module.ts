ac1ef83e3d9b21f4290476fefff7cb2edff72904
genadkogan <rgkogan@gmail.com>
2023-05-14 23:33:52 +0300

    sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { EducationComponent } from './education/education.component';
+import { ResumeComponent } from './resume/resume.component';
 import { HomePageComponent } from './home-page/home.page.component';
 import { PersonalDetailsComponent } from './personal-details/personal.details.component';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
+
 const appRoutes: Routes =[
   {path: '', component: HomePageComponent},
-  {path: 'education', component: EducationComponent},
+  {path: 'resume', component: ResumeComponent},
   {path: 'personal-details', component: PersonalDetailsComponent},
   { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
   { path: '**', redirectTo: '/not-found' }
@@ -21,7 +22,7 @@ const appRoutes: Routes =[
     AppComponent,
     HomePageComponent,
     PersonalDetailsComponent,
-    EducationComponent,
+    ResumeComponent,
     ErrorPageComponent
   ],
   imports: [
---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-education',
+    templateUrl: './resume.component.html',
+    styleUrls: ['./resume.component.css']
+
+})
+export class ResumeComponent{
+
+}
