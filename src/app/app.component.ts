289bf4c663561b797653555ff8a563b3b0319452
GenadyKogan <rgkogan@gmail.com>
2023-05-24 15:17:45 +0300

    responsive sidebar for all devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   activateSection(link: HTMLAnchorElement): void {
     const navLinks = document.querySelectorAll('nav ul li a');
     navLinks.forEach((item) => item.classList.remove('active'));
@@ -16,5 +16,15 @@ export class AppComponent {
     if (targetSection) {
       targetSection.classList.add('active');
     }*/
+  }
+  ngOnInit(): void {
+    const sidebarToggle = document.querySelector('#sidebar .toggle-sidebar');
+    const sidebar = document.querySelector('#sidebar');
+
+    if (sidebarToggle && sidebar) {
+      sidebarToggle.addEventListener('click', () => {
+        sidebar.classList.toggle('open');
+      });
+    }
   }
 }
